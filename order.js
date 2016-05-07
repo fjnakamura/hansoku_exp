7df506c5701c44ed1c0da39841de119d80cc89c6
fjnakamura <m-nakamura@fjtex.co.jp>
2016-05-07 16:20:31 +0900

    動作テスト用に修正

--- order.js
+++ order.js
@@ -1,18 +1,21 @@
 $(function () {
+  var nyukinmachiTxt = "入金待ちなり";
+  var cancelTxt = "キャンセルなり";
   $("select[name='status']").change( function () {
     var mailTextArea = $("textarea[name='comments']");
-    switch (this.val) {
+    switch (this.value) {
 
       // ご入金待ち
-      case 7:
+      case "5":
+        mailTextArea.val(nyukinmachiTxt);
         break;
 
       // キャンセル
       case 8:
+        mailTextArea.val(cancelTxt);
         break;
-      default
+      default:
         break;
     }
-    mailTextArea.value();
   });
 });
